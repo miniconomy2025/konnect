9f9c5bfaba846add173991d716c49d7a7c9df261
Daniel van Zyl <daniel.vanzyl@bbd.co.za>
2025-08-03 11:38:27 +0200

    Switched to old post account styling

--- frontend/src/components/account/PostsGrid.tsx
+++ frontend/src/components/account/PostsGrid.tsx
@@ -4,28 +4,18 @@ import React from 'react';
 import { styles } from '@/styles/account';
 import { Post } from '@/types/post';
 import PostItem from './PostItem';
-import { ImagePost } from '../Post/ImagePost';
-import { TextPost } from '../Post/TextPost';
 
 interface PostsGridProps {
   posts: Post[];
 }
 
-  const renderPost = (post: Post) => {
-    switch (post.media.type) {
-      case 'text':
-        return <TextPost key={post.id} post={post} />;
-      case 'image':
-        return <ImagePost key={post.id} post={post} />;
-      default:
-        return <TextPost key={post.id} post={post} />;
-    }
-  };
 
 const PostsGrid: React.FC<PostsGridProps> = ({ posts }) => {
   return (
     <section style={styles.postsGrid}>
-        {posts.map(renderPost)}
+        {posts.map((post) =>
+            <PostItem key={post.id} post={post} />
+        )}
     </section>
   );
 };
