69bb7a004a5b39197e3c4ab0a3faa85dcaf5ae3e
Daniel van Zyl <daniel.vanzyl@bbd.co.za>
2025-08-04 22:28:05 +0200

    Image domains

--- frontend/src/components/Home/Navbar.tsx
+++ frontend/src/components/Home/Navbar.tsx
@@ -31,17 +31,6 @@ const NavBar = () => {
           </section>
         </Link>
 
-        <Link href="/Discover" style={styles.link}>
-          <section style={styles.navLink}>
-            <section style={isActive('/Discover') ? styles.iconWrapperActive : styles.iconWrapper}>
-              <Search size={24} />
-            </section>
-            <section style={isActive('/Discover') ? styles.labelActive : styles.label}>
-              Discover
-            </section>
-          </section>
-        </Link>
-
         <Link href="/AddPost" style={styles.link}>
             <div style={styles.link}>
             <section style={styles.navLink} onClick={handleCreatePost}>
@@ -54,13 +43,24 @@ const NavBar = () => {
             </section>
             </div>
         </Link>
-        
+
+        <Link href="/Discover" style={styles.link}>
+          <section style={styles.navLink}>
+            <section style={isActive('/Discover') ? styles.iconWrapperActive : styles.iconWrapper}>
+              <Search size={24} />
+            </section>
+            <section style={isActive('/Discover') ? styles.labelActive : styles.label}>
+              Discover
+            </section>
+          </section>
+        </Link>
+
         <Link href="/Account" style={styles.link}>
           <section style={styles.navLink}>
-            <section style={isActive('/account') ? styles.iconWrapperActive : styles.iconWrapper}>
+            <section style={isActive('/Account') ? styles.iconWrapperActive : styles.iconWrapper}>
               <User size={24} />
             </section>
-            <section style={isActive('/account') ? styles.labelActive : styles.label}>
+            <section style={isActive('/Account') ? styles.labelActive : styles.label}>
               Account
             </section>
           </section>
